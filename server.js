27bb039c9e912f070730ea6647a2aa519b2f231c
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-09-14 00:19:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,18 +8,36 @@ const PORT = 8080;
 const app = express();
 const url = "http://browser-service:8080"
 
+var options = {
+    url: url
+};
+
 app.get('/', function (req, res) {
   res.send('Welcome to OpenShift!! My HostName is: ' + os.hostname() + '\n');
   console.log('HOSTNAME is' + os.hostname());
 
 });
 
-app.get('/browser', function (req, res) {
- 
-  console.log('insideeeeeeee');
-   request.get(url, (error, res, body) => {
-        return callback(error, res, body);
-    });
+
+app.get('/as', function (req, res) {
+  res.send('check consoleeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+  console.log('HOSTNAME is' + os.hostname());
+  
+  request(options, function(err, response, body) {
+    
+        if(err){
+        return console.log('Error:', err);
+    }
+
+    if(response.statusCode != 200){
+           res.jsonp(response.statusCode,body);
+    }
+
+   var body2 = JSON.parse(JSON.stringify(body));
+
+  console.log("body2 is," +body2);
+  });
+  
 
 });
 

e1027e86d52b67f051aa575b427eb0d88d93b9d5
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-09-13 23:58:29 +0530

    added

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+const http = require('http');
+const request = require('request');
+const util = require('util');
 var express = require('express');
 var os = require("os");
 //var cors = require('cors');
 const PORT = 8080;
 const app = express();
+const url = "http://browser-service:8080"
 
 app.get('/', function (req, res) {
   res.send('Welcome to OpenShift!! My HostName is: ' + os.hostname() + '\n');
   console.log('HOSTNAME is' + os.hostname());
+
+});
+
+app.get('/browser', function (req, res) {
+ 
+  console.log('insideeeeeeee');
+   request.get(url, (error, res, body) => {
+        return callback(error, res, body);
+    });
 
 });
 
